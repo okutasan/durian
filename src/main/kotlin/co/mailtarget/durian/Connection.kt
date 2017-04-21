b9db1d12b19f29da6869394ad231928d8363f728
masasdani <masasdn@gmail.com>
2017-04-21 20:41:50 +0700

    change connection throw to durian exception

--- src/main/kotlin/co/mailtarget/durian/Connection.kt
+++ src/main/kotlin/co/mailtarget/durian/Connection.kt
@@ -2,7 +2,6 @@ package co.mailtarget.durian
 
 import com.machinepublishers.jbrowserdriver.JBrowserDriver
 import com.machinepublishers.jbrowserdriver.Settings
-import com.sun.org.apache.xpath.internal.operations.Bool
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.openqa.selenium.WebDriver
@@ -19,14 +18,20 @@ open class Connection {
     val CONNECTION_TIMEOUT = 30000
     private val USER_AGENT = "Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/_BuildID_) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36";
 
-    @Throws(Exception::class)
     @JvmOverloads fun getDocument(url: String, timeout: Int = CONNECTION_TIMEOUT): Document {
-        return Jsoup.connect(url).userAgent(USER_AGENT).timeout(timeout).get()
+        try {
+            return Jsoup.connect(url).userAgent(USER_AGENT).timeout(timeout).get()
+        }catch (e: Exception) {
+            throw DurianException(e.message)
+        }
     }
 
-    @Throws(Exception::class)
     fun getDocument(url: String, html: String): Document {
-        return Jsoup.parse(html, url)
+        try {
+            return Jsoup.parse(html, url)
+        }catch (e: Exception) {
+            throw DurianException(e.message)
+        }
     }
 
     fun getWebDriver(logging: Boolean): WebDriver {
--- src/main/kotlin/co/mailtarget/durian/DurianException.kt
+++ src/main/kotlin/co/mailtarget/durian/DurianException.kt
@@ -5,6 +5,6 @@ package co.mailtarget.durian
  * @author masasdani
  * @since 4/7/17
  */
-class DurianException(override val message: String) : Exception(message){
+class DurianException(override val message: String?) : Exception(message){
     constructor(): this("Extract Exception")
 }
